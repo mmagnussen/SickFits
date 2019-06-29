5db0e26465bd3b3b62ada6890fc2765af1fc69b5
Mike <mike.magnussen@lifeway.com>
2019-06-29 15:38:19 -0500

    complete initial styling of Page and Header components

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6495014cf6f190693534";
+/******/ 	var hotCurrentHash = "0f7a60915f9d788f996a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 //importing React.Component doesn't really matter too much
+//build styled components where used, and if needed, move them to their own file to be available elsewhere
+
+//develop a theme that will have variables that can be applied anywhere
+const theme = {
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightgrey: '#E1E1E1',
+    offWhite: '#EDEDED',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+  `;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  
+  margin: 0 auto;
+  padding: 2rem;
+  `;
+
+
 class Page extends Component {
     render() {
         return (
-            <div>
-                <Meta />
-                <Header />
-                {this.props.children}
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta />
+                    <Header />
 
-            </div>
+                    <Inner>{this.props.children}</Inner>
+
+                </StyledPage>
+            </ThemeProvider>
         )
     }
 }
